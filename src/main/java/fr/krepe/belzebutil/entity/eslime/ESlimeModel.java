5fd0162f9600871376d1588567a89f2d7f00169c
Hugo Vilette <75722687+Krepe-Divine@users.noreply.github.com>
2022-10-12 10:52:05 +0200

    Update

---
+++ src/main/java/fr/krepe/belzebutil/entity/eslime/ESlimeModel.java
@@ -0,0 +1,11 @@
+package fr.krepe.belzebutil.entity.eslime;
+
+import fr.krepe.belzebutil.entity.ESlimeEntity;
+import net.minecraft.client.model.SlimeModel;
+import net.minecraft.client.model.geom.ModelPart;
+
+public class ESlimeModel extends SlimeModel<ESlimeEntity> {
+    public ESlimeModel(ModelPart p_170955_) {
+        super(p_170955_);
+    }
+}
